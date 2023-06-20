ab0f894235516a64c36b93065471f30c9abadd81
Edward Ndlala <edwardthina@gmail.com>
2023-06-20 22:56:23 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -8,13 +19,32 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        fs_objects = {}
+        if cls:
+            if type(cls) == str and cls in classes:
+                for key, val in self.__objects.items():
+                    if cls == key.split('.')[0]:
+                        fs_objects[key] = val
+            elif cls.__name__ in classes:
+                for key, val in self.__objects.items():
+                    if cls.__name__ == key.split('.')[0]:
+                        fs_objects[key] = val
+        else:
+            return self.__objects
+        return fs_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+    def delete(self, obj=None):
+        """Delete new instance """
+        if obj != None:
+           key = obj.__class__.__name__ + "." + str(obj.id)
+           if key in self.__objects:
+                del self.__objects[key]
+        self.save()
 
     def save(self):
         """Saves storage dictionary to file"""
